a97bd0acd8aa709b89191d30f9191fb2d063ba70
steve-anunknown <steve.anunknown@gmail.com>
2024-03-05 16:40:53 +0200

    made it prettier and now it broadcasts correctly

--- src/CLI.hs
+++ src/CLI.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CLI
   ( CLIInfo (..),
     CLISharedState,
@@ -12,18 +14,18 @@ import Control.Monad.Reader (MonadIO (liftIO), ReaderT, asks)
 import Data.IORef (IORef, readIORef)
 import qualified Data.Map as Map
 import GHC.IO.Handle (hFlush)
-import Network.Simple.TCP (HostName, ServiceName, connect, send)
+import Network.Simple.TCP (HostName, ServiceName)
 import ServiceType (ServiceType (..))
 import System.IO (stdout)
-import Transaction (Transaction (..), createTransaction)
-import Utils (encodeStrict)
+import Transaction (Transaction (..), createTransaction, broadcastTransaction, zeropub)
 import Wallet (Wallet)
 
 data CLIInfo = CLIInfo
   { cliWallet :: Wallet, -- Wallet of the user
-    cliPeers :: Map.Map Int PublicKey, -- Peers of the user
+    cliIDtoKey :: Map.Map Int PublicKey, -- Peers of the user
     cliNodeIP :: HostName, -- Node IP of the user
-    cliNodePort :: ServiceName -- Node Port of the user
+    cliNodePort :: ServiceName, -- Node Port of the user
+    cliPeers :: [(HostName, ServiceName)]
   }
   deriving (Show)
 
@@ -32,19 +34,26 @@ type CLISharedState = (IORef Blockchain, IORef Account)
 -- | Send a transaction to the network
 sendTx :: Int -> ServiceType -> Account -> ReaderT CLIInfo IO ()
 sendTx recvID service myacc = do
-  ip <- asks cliNodeIP
-  port <- asks cliNodePort
   peers <- asks cliPeers
+  keymap <- asks cliIDtoKey
   (pub, priv) <- asks cliWallet
   let mynonce = accountNonce myacc
-      recvPub = Map.lookup recvID peers
+      recvPub = Map.lookup recvID keymap
   case recvPub of
     Nothing -> liftIO $ putStrLn "Invalid recipient. Check whether the ID was yours or if it does not exist."
     Just somekey -> do
       let tx = createTransaction pub somekey service mynonce priv
-          msg = encodeStrict tx
       liftIO $ putStrLn $ "Sending " ++ show service ++ " to " ++ show recvID
-      liftIO $ connect ip port $ \(sock, _) -> send sock msg
+      liftIO $ broadcastTransaction peers tx -- this handles the correct sending
+
+stake :: Double -> Account -> ReaderT CLIInfo IO () 
+stake coins myacc = do
+  peers <- asks cliPeers
+  (pub, priv) <- asks cliWallet
+  let mynonce = accountNonce myacc
+      tx = createTransaction pub zeropub (Coins coins) mynonce priv
+  liftIO $ putStrLn $ "Staking " ++ show coins
+  liftIO $ broadcastTransaction peers tx
 
 -- | Handle the input from the user
 handle :: String -> CLISharedState -> ReaderT CLIInfo IO ()
@@ -54,16 +63,16 @@ handle input shared = do
   acc <- liftIO $ readIORef accref
   case tokens of
     ["t", num, "Coins", coins] -> sendTx (read num) (Coins (read coins)) acc
-    ["t", num, "Message", msg] -> sendTx (read num) (Message msg) acc
+    ("t" : num : "Message" : msgParts) -> sendTx (read num) (Message $ unwords msgParts) acc
     ["t", num, "Both", "(", coins, ",", msg, ")"] -> sendTx (read num) (Both (read coins, msg)) acc
-    ["stake", coins] -> sendTx 0 (Coins (read coins)) acc
+    ["stake", coins] -> stake (read coins) acc
     ["view"] -> do
       blockchain <- liftIO $ readIORef blockref
-      liftIO $ prettyPrintBlock (head blockchain)
+      prettyPrintBlock (head blockchain)
     ["balance"] -> liftIO $ print (accountBalance acc)
     ["peers"] -> do
-      peers <- asks cliPeers
-      liftIO $ prettyPrintPeers peers
+      keymap<- asks cliIDtoKey
+      liftIO $ prettyPrintPeers keymap
     ["help"] -> do
       liftIO $ putStrLn "t <recipient id> Coins <coins>          - send coins"
       liftIO $ putStrLn "t <recipient id> Message <msg>          - send message"
@@ -88,24 +97,36 @@ shell shared = do
       input <- liftIO getLine
       handle input shared >> loop
 
-prettyPrintBlock :: Block -> IO ()
+prettyPrintBlock :: Block -> ReaderT CLIInfo IO ()
 prettyPrintBlock block = do
-  putStrLn $ "Block Index: " ++ show (blockIndex block)
-  putStrLn $ "Block Timestamp: " ++ show (blockTimestamp block)
-  putStrLn "Block Transactions: "
+  idTokey <- asks cliIDtoKey
+  -- use the validator key to get its ID
+  let matches = Map.keys $ Map.filter (== blockValidator block) idTokey
+      validatorID = if null matches then "you" else show $ head matches
+  liftIO $ putStrLn "==================================================="
+  liftIO $ putStrLn $ "Block Index: " ++ show (blockIndex block)
+  liftIO $ putStrLn $ "Block Timestamp: " ++ show (blockTimestamp block)
+  liftIO $ putStrLn "Block Transactions: "
   prettyPrintTXs $ blockTransactions block
-  putStrLn $ "Block Validator: " ++ show (blockValidator block)
+  liftIO $ putStrLn $ "Block Validator: " ++ (if blockIndex block == 1 then "genesis" else validatorID)
+  liftIO $ putStrLn "==================================================="
 
-prettyPrintTX :: Transaction -> IO ()
+prettyPrintTX :: Transaction -> ReaderT CLIInfo IO ()
 prettyPrintTX Transaction {senderAddress = s, receiverAddress = r, serviceType = st, nonce = n} = do
-  putStrLn $ "Sender: " ++ show s
-  putStrLn $ "Receiver: " ++ show r
-  putStrLn $ "Service: " ++ show st
-  putStrLn $ "Nonce: " ++ show n
-  putStrLn "-------------------"
+  idToKey <- asks cliIDtoKey
+  let matchesS = Map.keys $ Map.filter (== s) idToKey
+      matchesR = Map.keys $ Map.filter (== r) idToKey
+      sid = if null matchesS then "you" else show $ head matchesS
+      rid = if null matchesR then "you" else show $ head matchesR
+  liftIO $ putStrLn "\t|---------------------------------------------------"
+  liftIO $ putStrLn $ "\t|TX Sender: " ++ (if s == zeropub then "genesis" else sid)
+  liftIO $ putStrLn $ "\t|TX Receiver: " ++ (if s == zeropub then "bootstrap" else (if r == zeropub then "stake" else rid))
+  liftIO $ putStrLn $ "\t|TX Service: " ++ show st
+  liftIO $ putStrLn $ "\t|TX Nonce: " ++ show n
+  liftIO $ putStrLn "\t|---------------------------------------------------"
 
-prettyPrintTXs :: [Transaction] -> IO ()
+prettyPrintTXs :: [Transaction] -> ReaderT CLIInfo IO ()
 prettyPrintTXs = mapM_ prettyPrintTX
 
 prettyPrintPeers :: Map.Map Int PublicKey -> IO ()
-prettyPrintPeers = print . unlines . map (\(i, _) -> show "Node with ID " ++ show i) . Map.toList
+prettyPrintPeers = print . unlines . map (\(i, _) -> "Node with ID " ++ show i) . Map.toList
