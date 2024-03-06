69ff3084be3cffa84c8cc938b0199120d47fc9af
steve-anunknown <steve.anunknown@gmail.com>
2024-03-06 11:24:51 +0200

    added documentation and modified the updateStake function so that it is possible to restake

--- src/Account.hs
+++ src/Account.hs
@@ -4,10 +4,9 @@ module Account
     availableBalance,
     updateBalanceBy,
     updateNonce,
-    updateStakeBy
+    updateStake,
   )
 where
-
 
 data Account = Account
   { accountBalance :: Double, -- the balance of the account.
@@ -21,17 +20,29 @@ data Account = Account
   }
   deriving (Show, Eq)
 
+-- | The initial account has a balance of 1000 coins, a nonce of 0 and no stake.
 initialAccount :: Account
 initialAccount = Account 1000 0 0
 
+-- | The available balance of an account is the balance minus the stake.
 availableBalance :: Account -> Double
 availableBalance Account {accountBalance = bal, accountStake = st} = bal - st
 
+-- | This function takes an amount and an account as arguments and returns a new account
+-- with the balance updated by the amount.
 updateBalanceBy :: Double -> Account -> Account
-updateBalanceBy amount acc  = acc {accountBalance = accountBalance acc + amount}
+updateBalanceBy amount acc = acc {accountBalance = accountBalance acc + amount}
 
+-- | This function takes an account as an argument and returns a new account with the nonce
+-- incremented by 1.
 updateNonce :: Account -> Account
 updateNonce acc = acc {accountNonce = accountNonce acc + 1}
 
-updateStakeBy :: Double -> Account -> Account
-updateStakeBy amount acc = acc {accountStake = accountStake acc + amount}
+-- | This function takes an amount and an account as arguments and returns a new account
+-- with the stake updated by the amount.
+updateStake :: Double -> Account -> Account
+updateStake amount acc =
+  acc
+    { accountStake = amount,
+      accountBalance = accountBalance acc + (accountStake acc - amount)
+    }
