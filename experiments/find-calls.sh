a31ae7331aca67767251feab03a56c1708031526
steve-anunknown <steve.anunknown@gmail.com>
2024-03-10 12:59:12 +0200

    fixed some minor errors

--- experiments/find-calls.sh
+++ experiments/find-calls.sh
@@ -12,6 +12,17 @@ do
     echo "Processing $experiment"
     for folder in "$experiment"/*;
     do
+        # search the file from the second "COST CENTRE" to the end
         grep "$pattern" "$folder"/*.prof.prof > "$folder"/calls.txt
+        # replace every occurence of multiple spaces with a comma
+        # followed by a space
+        sed -i 's/ \+/,\ /g' "$folder"/calls.txt
+        sed -i "s/${directory}\///g" "$folder"/calls.txt
+        # delete the pattern that starts with 'src' and
+        # ends with '),'.
+        sed -i 's/src.*),//g' "$folder"/calls.txt
+        # if there is a line with ':' followed by an alphabetical
+        # character, delete it
+        sed -i '/:[a-zA-Z]/d' "$folder"/calls.txt
     done
 done
