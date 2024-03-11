cccecd8437469a24e98634b9ae8e96cd2ec563b4
steve-anunknown <steve.anunknown@gmail.com>
2024-03-11 10:29:10 +0200

    does some filtering and creates a file just for the report in latex

--- experiments/find-calls.sh
+++ experiments/find-calls.sh
@@ -24,5 +24,23 @@ do
         # if there is a line with ':' followed by an alphabetical
         # character, delete it
         sed -i '/:[a-zA-Z]/d' "$folder"/calls.txt
+
+
+
+        # create a new file called calls-latex.txt
+        cp "$folder"/calls.txt "$folder"/calls-latex.txt
+        # every line starts with the path of the file
+        # delete it and keep only the node<num>.prof
+        sed -i 's/.*\///' "$folder"/calls-latex.txt
+        # match node<num>.prof.prof and replace it
+        # with node<num>.prof
+        sed -i 's/.prof.prof:/.prof/' "$folder"/calls-latex.txt
+        # prepend to the first line of the file
+        # the string "File, Function, Module, Number, Calls, Time-Ind, Mem-Ind, Time-Inh, Mem-Inh"
+        sed -i '1s/^/File, Function, Module, Number, Calls, TimeInd, MemInd, TimeInh, MemInh\n/' "$folder"/calls-latex.txt
+        sed -i "s/nodeLogic\.//g" "$folder"/calls-latex.txt
+        sed -i "s/processTXs\.//g" "$folder"/calls-latex.txt
+        # keep only columns 1, 2, 5, 8, 9
+        awk -F, '{print $1","$2","$5","$8","$9}' "$folder"/calls-latex.txt > "$folder"/calls-latex.csv
     done
 done
