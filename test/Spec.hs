37d76c2932adb9744ca5b9d26e2572626d02de6e
steve-anunknown <steve.anunknown@gmail.com>
2024-04-10 13:11:00 +0300

    added more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,10 +10,39 @@ import Network.Simple.TCP (HostName, ServiceName, connect, send, sendLazy, recv)
 import OrdinaryNode (NodeInfo (..), nodeStartupTest, sampleValidator)
 import System.Directory (removeFile)
 import System.Random (mkStdGen)
-import Test.Hspec
+import Test.QuickCheck
+    ( Gen,
+      Arbitrary(arbitrary),
+      Testable(property),
+      listOf,
+      suchThat,
+      (==>),
+      forAll )
+import Test.Hspec ( hspec, describe, it, shouldSatisfy, shouldBe )
 import Wallet (generateWallet)
 import Utils (receiveChunks)
 import Data.Maybe (fromMaybe)
+import qualified Statistics.Sample as Statistics
+import qualified Data.Vector as V
+
+round4 :: Double -> Double
+round4 x = fromIntegral (round $ x * 1e4) / 1e4
+
+-- Custom generator for lists of Doubles greater than 0
+positiveDoubles :: Gen [Double]
+positiveDoubles = listOf (abs <$> arbitrary `suchThat` (> 0))
+
+arithmetic :: Fractional a => [a] -> a
+arithmetic numbers = totalsum / amount
+    where (totalsum, amount) = foldr (\num (a, b) -> (num + a, b + 1)) (0, 0) numbers
+
+harmonic :: Fractional a => [a] -> a
+harmonic numbers = amount / inversesum
+  where (inversesum, amount) = foldr (\num (a, b) -> (a + 1 / num, b + 1)) (0, 0) numbers
+
+geometric :: Floating a => [a] -> a
+geometric numbers = product numbers ** (1 / amount)
+  where amount = fromIntegral $ length numbers
 
 main :: IO ()
 main = hspec $ do
@@ -50,7 +79,7 @@ main = hspec $ do
       let limit = 10
       result <- connect "127.0.0.1" "35000" $ \(socket, _) -> do
         sendLazy socket "Hello"
-        receiveChunks socket limit   
+        receiveChunks socket limit
       result `shouldBe` "Hello"
 
     it "returns the first 'limit' bytes when the total length is greater than the limit" $ do
@@ -66,7 +95,7 @@ main = hspec $ do
         -- or program into it some kind of timeout to exit
         receiveChunks socket limit
       result `shouldBe` ""
-    
+
   describe "receiveChunks against recv" $ do
     it "should return the same result as recv (msg less than limit)" $ do
       let limit = 10
@@ -97,7 +126,21 @@ main = hspec $ do
         -- or program into it some kind of timeout to exit
         fromMaybe "" <$> recv socket limit
       res1 `shouldBe` res2
-    
+
+  describe "arithmeticMean" $ do
+    it "returns the same result as Statistics.mean" $ property $
+      \list -> not (null (list :: [Double])) ==>
+        round4 (arithmetic list) `shouldBe` round4 (Statistics.mean (V.fromList list))
+
+  describe "harmonicMean" $ do
+    it "returns the same result as Statistics.harmonicMean" $ property $
+      \list -> not (null (list :: [Double])) && notElem 0 list ==>
+        round4 (harmonic list) `shouldBe` round4 (Statistics.harmonicMean (V.fromList list))
+
+  describe "geometricMean" $ do
+    it "returns the same result as Statistics.geometricMean" $ property $
+      forAll positiveDoubles $ \list -> not (null list) ==>
+        round4 (geometric list) `shouldBe` round4 (Statistics.geometricMean (V.fromList list))
 
   describe "Node Startup Test" $ do
     it "checks if the node starts up correctly" $ do
